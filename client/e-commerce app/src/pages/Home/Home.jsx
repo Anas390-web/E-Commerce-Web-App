cc2e182fe1cb52e43453cde4db93be997f08c3da
anas <anasrehman390@gmail.com>
2026-07-07 23:26:12 +0500

    main branch: 5th commit: Hero section is done in Home page Component. Added an image for testing.

--- client/e-commerce app/src/pages/Home/Home.jsx
+++ client/e-commerce app/src/pages/Home/Home.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import './home.css'
+import HeadphonesImage from './../../images/headphones-black.png';
 
 function Home() {
   return (
-    <div>Home</div>
+    <main>
+      <section className='hero-section'>
+        <div className='hero-container'>
+          <div className='hero-container-text'>
+            <div className='hero-container-text-heading'>
+              <p>Upgrade Your Setup, Elevate Your Performance!</p>
+            </div>
+            <div className='hero-container-text-paragraph'>
+              <p>Explore our curated collection of premium computer accessories and cutting-edge gear tailored to power your daily grind and ultimate gaming sessions.</p>
+            </div>
+            <div className='hero-container-btn'>
+              <button>EXPLORE NOW</button>
+            </div>
+          </div>
+          <div className='hero-container-image'>
+            <img src={HeadphonesImage} alt="Headphones" />
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
 
