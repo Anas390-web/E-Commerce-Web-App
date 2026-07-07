6def3e18028f7d762fe89a4880806552a69413d0
anas <anasrehman390@gmail.com>
2026-07-07 18:24:43 +0500

    main branch: 3rd commit: Set up the React Router for Navigation. Navigation is done in NavBar component.

--- client/e-commerce app/src/Components/NavBar/NavBar.jsx
+++ client/e-commerce app/src/Components/NavBar/NavBar.jsx
@@ -1,3 +1,34 @@
+import { NavLink } from 'react-router'
+
 export default function NavBar() {
-    
+   return (
+      <nav>
+         <div className="nav-bar">
+            <ul>
+               <li>
+                  <NavLink
+                     to="/"
+                     className={({ isActive }) => { isActive ? "#224F34" : "green" }}
+                  >
+                     HOME
+                  </NavLink>
+               </li>
+               <li>
+                  <NavLink
+                     to="/about"
+                     className={({ isActive }) => { isActive ? "#224F34" : "green" }}>
+                     ABOUT
+                  </NavLink>
+               </li>
+               <li>
+                  <NavLink
+                     to="/products"
+                     className={({ isActive }) => { isActive ? "#224F34" : "green" }}>
+                     PRODUCTS
+                  </NavLink>
+               </li>
+            </ul>
+         </div>
+      </nav>
+   )
 }
--- client/e-commerce app/src/main.jsx
+++ client/e-commerce app/src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router'
+import Layout from './Components/Layout/Layout.jsx'
+import About from './pages/About/About.jsx'
+import Home from './pages/Home/Home.jsx'
+import Products from './pages/Products/Products.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />}/>
+      <Route path='/about' element={<About />}/>
+      <Route path='/products' element={<Products />}/>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
