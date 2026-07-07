3b82bcaf8459d443619020f6a7d098f312ac7c94
anas <anasrehman390@gmail.com>
2026-07-07 20:53:30 +0500

    main branch: 4th commit: NavBar Component initial structure and styling is done.

--- client/e-commerce app/src/Components/NavBar/NavBar.jsx
+++ client/e-commerce app/src/Components/NavBar/NavBar.jsx
@@ -1,34 +1,51 @@
-import { NavLink } from 'react-router'
+import { NavLink, Link } from 'react-router'
+import { CartIcon } from '../../icons/Icons'
+import './navBar.css'
 
 export default function NavBar() {
    return (
-      <nav>
-         <div className="nav-bar">
-            <ul>
-               <li>
-                  <NavLink
-                     to="/"
-                     className={({ isActive }) => { isActive ? "#224F34" : "green" }}
-                  >
-                     HOME
-                  </NavLink>
-               </li>
-               <li>
-                  <NavLink
-                     to="/about"
-                     className={({ isActive }) => { isActive ? "#224F34" : "green" }}>
-                     ABOUT
-                  </NavLink>
-               </li>
-               <li>
-                  <NavLink
-                     to="/products"
-                     className={({ isActive }) => { isActive ? "#224F34" : "green" }}>
-                     PRODUCTS
-                  </NavLink>
-               </li>
-            </ul>
-         </div>
-      </nav>
+      <header>
+         <nav>
+            <div className="nav-bar-container">
+               <div className='nav-title'>
+                  <h2>BuyMe</h2>
+               </div>
+               <div className='navLinks'>
+                  <ul>
+                     <li>
+                        <NavLink
+                           to="/"
+                           style={({ isActive }) => ({ backgroundColor: isActive ? "#6BC785" : "#C2EFD4" })}>
+                           HOME
+                        </NavLink>
+                     </li>
+                     <li>
+                        <NavLink
+                           to="/about"
+                           style={({ isActive }) => ({ backgroundColor: isActive ? "#6BC785" : "#C2EFD4" })}>
+                           ABOUT
+                        </NavLink>
+                     </li>
+                     <li>
+                        <NavLink
+                           to="/products"
+                           style={({ isActive }) => ({ backgroundColor: isActive ? "#6BC785" : "#C2EFD4" })}>
+                           PRODUCTS
+                        </NavLink>
+                     </li>
+                  </ul>
+               </div>
+               <div className='links'>
+                  <Link
+                     to="/cart" >
+                     <CartIcon />
+                  </Link>
+               </div>
+               <div className='nav-login-btn'>
+                  <button>LOGIN</button>
+               </div>
+            </div>
+         </nav>
+      </header>
    )
 }
--- client/e-commerce app/src/main.jsx
+++ client/e-commerce app/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router'
 import Layout from './Components/Layout/Layout.jsx'
 import About from './pages/About/About.jsx'
 import Home from './pages/Home/Home.jsx'
 import Products from './pages/Products/Products.jsx'
+import Cart from './pages/Cart/Cart.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -14,6 +14,7 @@ const router = createBrowserRouter(
       <Route path='' element={<Home />}/>
       <Route path='/about' element={<About />}/>
       <Route path='/products' element={<Products />}/>
+      <Route path='/cart' element={<Cart />} />
     </Route>
   )
 )
