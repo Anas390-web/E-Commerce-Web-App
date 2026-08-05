138171116e020b734f1c5a9e24a76eeb616e935c
anas <anasrehman390@gmail.com>
2026-08-05 23:25:09 +0500

    feat(custom-errors): add CustomApiError, BadRequestError, UnauthenticatedError error classes

---
+++ server/errors/custom-errors.js
@@ -0,0 +1,23 @@
+class CustomApiError extends Error {
+   constructor(message, statusCode) {
+      super(message),
+         this.statusCode = statusCode
+   }
+}
+
+class BadRequestError extends CustomApiError {
+   constructor(message, statusCode) {
+      super(message),
+         this.statusCode = 400
+   }
+}
+
+class UnauthenticatedError extends CustomApiError {
+   constructor(message, statusCode) {
+      super(message),
+         this.statusCode = 404
+   }
+}
+
+
+export { CustomApiError, BadRequestError, UnauthenticatedError }
