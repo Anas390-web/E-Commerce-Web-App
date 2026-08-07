d7d19d74c1c9e39fe6cee1cd20a8d79c3ceb0437
anas <anasrehman390@gmail.com>
2026-08-07 21:36:05 +0500

    feat(routes/users): set route for getUserDetails()
    Set a route for a callback to respond get request fetchUserDetails() asyncthunk. Import authentication callback from the middlewares/auth.js and add it before getUserDetails() callback to check if the token is verified and to get user details from token payload.

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { signUpUser, login } from '../controllers/auth.js';
+import { signUpUser, login, getUserDetails } from '../controllers/auth.js';
+import authentication from '../middlewares/auth.js'
 const authRouter = express.Router();
 
 authRouter.route('/register').post(signUpUser)
 authRouter.route('/login').post(login)
+authRouter.route('/').get(authentication ,getUserDetails)
 
 export default authRouter
