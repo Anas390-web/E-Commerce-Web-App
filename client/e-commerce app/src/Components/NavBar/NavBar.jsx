7c6d9d3f925f6875dbd5b4e9778a4d9971898dba
anas <anasrehman390@gmail.com>
2026-08-07 21:06:59 +0500

    feat(NavBar): logout button & user/guest mode
    Create a logout button and dispatch logout() action to navigate user
    back to login page.
    Dispatch fetchUserDetails() (async thunk) that lives in authSlice to show
    username upon re-loading on Navbar component.

--- client/e-commerce app/src/Components/NavBar/NavBar.jsx
+++ client/e-commerce app/src/Components/NavBar/NavBar.jsx
@@ -1,8 +1,28 @@
 import { NavLink, Link } from 'react-router'
 import { CartIcon } from '../../icons/Icons'
 import './navBar.css'
+import { useNavigate } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUserDetails, logout } from '../../features/Authentication/authSlice';
+import { useEffect } from 'react';
 
 export default function NavBar() {
+   const navigate = useNavigate();
+   const dispatch = useDispatch();
+   // ACCESSING USERNAME FROM AUTH SLICE FROM STORE:
+   const { username } = useSelector((store) => store.auth);
+   // TO DISPLAY USERNAME UPON RE-LOADING PAGE:
+   useEffect(() => {
+      dispatch(fetchUserDetails())
+   }, [dispatch])
+   // ACCESSING TOKEN:
+   const token = localStorage.getItem('accessToken');
+   // TO NAVIGATE BACK TO LOGIN PAGE IF USER LOGGED-OU:T
+   function handleLogOutClick(e) {
+      e.preventDefault();
+      dispatch(logout());
+      navigate('/login');
+   }
    return (
       <header>
          <nav>
@@ -41,13 +61,25 @@ export default function NavBar() {
                      <CartIcon />
                   </Link>
                </div>
-               <div className='nav-login-btn'>
-                  <Link to='/login'>
-                     <button id='login'>Login</button>
-                  </Link>
-                  <Link to='/register'>
-                     <button id='sign-up'>Sign Up</button>
-                  </Link>
+               <div className='nav-auth-btn'>
+                  {token && token.length > 0 ?
+                     <>
+                        <div id='username'>
+                           <p>Hi, {username.toUpperCase()}</p>
+                        </div>
+                        <button onClick={handleLogOutClick} id='logout'>Logout</button>
+                     </>
+                     :
+                     <>
+                        <Link to='/login'>
+                           <button id='login'>Login</button>
+                        </Link>
+                        <Link to='/register'>
+                           <button id='sign-up'>Sign Up</button>
+                        </Link>
+                     </>
+
+                  }
                </div>
             </div>
          </nav>
