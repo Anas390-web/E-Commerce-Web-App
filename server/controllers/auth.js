aaa7a67c1720ab149dd0aa2cce31b15405ae99d0
anas <anasrehman390@gmail.com>
2026-08-07 21:32:20 +0500

    feat(auth): create callback to respond get req
    Create a callback function to respond async thunk fetchUserDetails and
    send json data with username in it.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -6,12 +6,12 @@ const signUpUser = async (req, res) => {
    const { username, email, password } = req.body;
    // THROW ERROR IF THEY DON'T EXIST:
    if (!username || !email || !password) {
-      return res.status(StatusCodes.BAD_GATEWAY).json({
+      return res.status(StatusCodes.BAD_REQUEST).json({
          msg: 'Please provide credentials'
       })
    }
    // SAVE THE CREDENTIALS IN DB:
-   const user = await User.create({ ...req.body });
+   const user = await User.create({ username, email, password });
    // CREATE A TOKEN FOR THE USER:
    const token = user.createJwt();
    // RESPOND TO USER WITH TOKEN AND USERNAME:
@@ -42,7 +42,8 @@ const login = async (req, res) => {
    const isPasswordCorrect = await user.comparePassword(password);
    // THROW AN ERROR IF PASSWORD IS NOT CORRECT/MATCHED:
    if (!isPasswordCorrect) {
-      return res.status(StatusCodes.NOT_FOUND).json({
+      console.log('Password is not correct');
+      return res.status(StatusCodes.UNAUTHORIZED).json({
          msg: 'Password is not correct'
       })
    }
@@ -52,4 +53,17 @@ const login = async (req, res) => {
    res.status(StatusCodes.OK).json({ username: user.username, token })
 }
 
-export { signUpUser, login }
+const getUserDetails = async (req, res) => {
+   const { username, userId } = req.user;
+   const user = await User.findOne({_id: userId, username});
+   if(!user) {
+      return res.status(StatusCodes.NOT_FOUND).json({
+         msg: 'User not found'
+      })
+   }
+   res.status(StatusCodes.OK).json({
+      username: user.username
+   })
+}
+
+export { signUpUser, login, getUserDetails }
