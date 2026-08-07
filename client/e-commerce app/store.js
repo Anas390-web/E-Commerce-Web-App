c56d65a6a19aa44f9bad65c9dd5e2fbf52906b6b
anas <anasrehman390@gmail.com>
2026-08-07 21:04:21 +0500

    feat(store): add auth state in store
    Add the authSlice state in the global state management store so can be accessed accross auth components and others.

--- client/e-commerce app/store.js
+++ client/e-commerce app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { productReducer } from "./src/features/Products-List/productListSlice";
 import { cartReducer } from "./src/features/Cart/cartSlice";
+import authReducer from "./src/features/Authentication/authSlice";
 export const store = configureStore({
    reducer: {
       productList: productReducer,
-      cart: cartReducer
+      cart: cartReducer,
+      auth: authReducer
    }
 })
