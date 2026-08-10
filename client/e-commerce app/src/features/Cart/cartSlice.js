4425d6e56b5812327b7feb46bb0781331a9fdade
anas <anasrehman390@gmail.com>
2026-08-10 21:24:33 +0500

    feat(cartSlice): Add GET,POST,DELETE Async thunks
    Create saveUserCartItem() async thunk to post user cart items and their quantities in the DB and add extraReducers to change the UI state.
    Create getUserCart() async thunk to get user cart items and their
    quantities from the server/DB and add extraReducers to change the UI
    state.
    Create deleteUserCartItem() async thunk to delete the user cart item
    from the server/DB and add extraReducers to change the UI state.
    Create clearUserCart() async thunk to request empty the user cart from
    the server/DB and add extraReducers to change the UI state.
    Change the properties in increment and decrement reducers from .id to
    .product._id and .productCount to .quantity.
    Add the comment over addToCart, deleteItem, clearCart reducers:
    "USED TO BUILT FOR FRONTEND, NOW BACKEND TAKES CARE OF IT"

--- client/e-commerce app/src/features/Cart/cartSlice.js
+++ client/e-commerce app/src/features/Cart/cartSlice.js
@@ -1,74 +1,250 @@
-import { createSelector, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSelector, createSlice } from "@reduxjs/toolkit";
 
-// TWO SELECTORS TO BE MERGED INTO A SELECTOR FUNCTION:
-const selectProductItems = (state) => state.productList.products;
-const selectCartItems = (state) => state.cart.cartItems;
+// IMPORTANT:
+// THIS WAS NEEDED BACK THEN TO SHOW CART ITEMS IN THE CART COMPONENT SINCE CART ITEMS ONLY HAD TWO PROPERTIES PRODUCTID AND PRODUCT COUNT AND WE OVERWRITED THEM WITH PRODUCT PROPERTIES FROM THE PRODUCTS PRODUCTLIST NOW WE ARE DEALING WITH THAT WITH BACKEND WE DON'T NEED IT ANYMORE:
+// // TWO SELECTORS TO BE MERGED INTO A SELECTOR FUNCTION:
+// const selectProductItems = (state) => state.productList.products;
+// const selectCartItems = (state) => state.cart.cartItems;
 
-// CREATING A SELECTOR FUNCTION WHICH MERGES TWO STATES AND RETURNS ONE OUTPUT:
-export const selectCartItemWithProductDetails = createSelector(
-   [selectProductItems, selectCartItems], // input selectors
-   // return values of each input selector
-   (products, cartItems) => {
-      return cartItems.map((cartItem) => {
-         const matchingProduct =  products.find((product) => {
-            return product._id === cartItem.productId
+// // CREATING A SELECTOR FUNCTION WHICH MERGES TWO STATES AND RETURNS ONE OUTPUT:
+// export const selectCartItemWithProductDetails = createSelector(
+//    [selectProductItems, selectCartItems], // input selectors
+//    // return values of each input selector
+//    (products, cartItems) => {
+//       return cartItems.map((cartItem) => {
+//          const matchingProduct =  products.find((product) => {
+//             return product._id === cartItem.productId
+//          })
+//          return {
+//             ...matchingProduct,
+//             productCount: cartItem.productCount,
+//          }
+//       })
+//    }
+// )
+
+// POST REQUEST TO REGISTER THE USER CART ITEM(s):
+export const saveUserCartItem = createAsyncThunk(
+   'cart/saveUserCartItem',
+   async (item, thunkApi) => {
+      try {
+         const token = await localStorage.getItem('accessToken');
+         const url = import.meta.env.VITE_SERVER_CART_URL;
+         const response = await fetch(`${url}/${item.productId}`, {
+            method: 'POST',
+            headers: {
+               'Authorization': `Bearer ${token}`, // USER ID WILL BE TAKEN FROM TOKEN PAYLOAD
+               'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+               product: item.productId,
+               quantity: item.productCount
+            })
          })
-         return {
-            ...matchingProduct,
-            productCount: cartItem.productCount,
+         const data = await response.json();
+         if (!response.ok) {
+            return thunkApi.rejectWithValue(data);
          }
-      })
+         return data;
+      } catch (error) {
+         console.log(error.message);
+         return thunkApi.rejectWithValue(error.message)
+      }
    }
 )
+
+// GET USER CART:
+export const getUserCart = createAsyncThunk(
+   'cart/getUserCart',
+   async (userData, thunkApi) => {
+      try {
+         const token = await localStorage.getItem('accessToken');
+         const url = import.meta.env.VITE_SERVER_CART_URL;
+         const response = await fetch(`${url}`, {
+            method: 'GET',
+            headers: {
+               'Authorization': `Bearer ${token}`,
+               'content-type': 'application/json'
+            }
+         })
+         const data = await response.json();
+         if (!response.ok) {
+            return thunkApi.rejectWithValue(data);
+         }
+         return data;
+      } catch (error) {
+         console.log(error.message);
+         return thunkApi.rejectWithValue(error.message)
+      }
+   }
+)
+
+// DELETE USER CART ITEM:
+export const deleteUserCartItem = createAsyncThunk(
+   'cart/deleteUserCartItem',
+   async (item, thunkApi) => {
+      try {
+         const token = localStorage.getItem('accessToken');
+         const response = await fetch(`${import.meta.env.VITE_SERVER_CART_URL}/${item.productId}`, {
+            method: 'DELETE',
+            headers: {
+               'Authorization': `Bearer ${token}`,
+               'content-type': 'application/json'
+            }
+         })
+         const data = await response.json();
+         if (!response.ok) {
+            return thunkApi.rejectWithValue(data);
+         }
+         return data;
+      } catch (error) {
+         console.log(error.message);
+         return thunkApi.rejectWithValue(error.message)
+      }
+   }
+)
+
+// CLEAR USER CART:
+export const clearUserCart = createAsyncThunk(
+   'cart/clearUserCart',
+   async (_, thunkApi) => {
+      try {
+         const token = await localStorage.getItem('accessToken');
+         const response = await fetch(`${import.meta.env.VITE_SERVER_CART_URL}`, {
+            method: 'DELETE',
+            headers: {
+               'Authorization': `Bearer ${token}`
+            }
+         })
+         const data = await response.json();
+         if(!response.ok) {
+            return thunkApi.rejectWithValue(data)
+         }
+         return data;
+      } catch (error) {
+         console.log(error);
+         return thunkApi.rejectWithValue(error)
+      }
+   }
+)
+
 const initialState = {
    cartItems: [],
    count: 0,
    totalAmount: 0,
    isLoading: true,
-   isAddedToCart: false
+   error: null
 }
 
 const cartSlice = createSlice({
    name: 'cart',
    initialState,
    reducers: {
+      // USED TO BUILT FOR FRONTEND, NOW BACKEND TAKES CARE OF IT
       addToCart: (state, action) => {
          state.count += action.payload.productCount;
          state.cartItems.push(action.payload);
       },
       increment: (state, action) => {
          const item = state.cartItems.find((cartItem) => {
-            return cartItem.productId === action.payload
-         })
-         if(item) {
-            item.productCount += 1
+            return cartItem.product._id === action.payload;
+         });
+         if (item) {
+            item.quantity += 1
          }
       },
       decrement: (state, action) => {
          const item = state.cartItems.find((cartItem) => {
-            return cartItem.productId === action.payload
+            return cartItem.product._id === action.payload
          })
-         if(item){
-            item.productCount > 0 ? item.productCount -= 1 : 1
+         if (item) {
+            item.quantity > 1 ? item.quantity -= 1 : 1
          }
       },
+      // USED TO BUILT FOR FRONTEND, NOW BACKEND TAKES CARE OF IT
       deleteItem: (state, action) => {
-         const items = state.cartItems.filter((product) => {
-            return product.productId !== action.payload
+         const items = state.cartItems.filter((cartItem) => {
+            return cartItem.product._id !== action.payload
          })
          state.cartItems = items;
 
          // IF ITEMS REMOVED THAN CART COUNT SHOULD REMOVE AS WELL:
-         let remainigProductsCount = 0;
-         items.forEach((product) => {
-            remainigProductsCount += product.productCount;
+         let remainingItemsQuantity = 0;
+         items.forEach((item) => {
+            return remainingItemsQuantity += item.quanity;
          })
-         state.count = remainigProductsCount;
+         state.count = remainingItemsQuantity;
       },
+      // USED TO BUILT FOR FRONTEND, NOW BACKEND TAKES CARE OF IT
       clearCart: (state, action) => {
          state.cartItems = [];
          state.count = 0;
       }
+   },
+   extraReducers: (builder) => {
+      builder
+         // SAVE USER SELECTED ITEM TO CART:
+         .addCase(saveUserCartItem.pending, (state) => {
+            state.isLoading = true;
+         })
+         .addCase(saveUserCartItem.fulfilled, (state, action) => {
+            state.isLoading = false;
+            const quantity = action.payload.cart.items.reduce((total, currentItem) => {
+               return total += currentItem.quantity
+            }, 0)
+            state.count = quantity;
+            state.cartItems = action.payload.cart.items
+         })
+         .addCase(saveUserCartItem.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.payload;
+         })
+         // GET USER CART:
+         .addCase(getUserCart.pending, (state) => {
+            state.isLoading = true;
+         })
+         .addCase(getUserCart.fulfilled, (state, action) => {
+            state.isLoading = false;
+            const quantity = action.payload.cart.items.reduce((total, currentItem) => {
+               return total += currentItem.quantity
+            }, 0)
+            state.count = quantity;
+            state.cartItems = action.payload.cart.items
+         })
+         .addCase(getUserCart.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.payload;
+         })
+         // DELETE USER SINGLE PRODUCT FROM CART:
+         .addCase(deleteUserCartItem.pending, (state) => {
+            state.isLoading = true;
+         })
+         .addCase(deleteUserCartItem.fulfilled, (state, action) => {
+            state.isLoading = false;
+            const quantity = action.payload.cart.items.reduce((total, currentItem) => {
+               return total += currentItem.quantity
+            }, 0)
+            state.count = quantity;
+            state.cartItems = action.payload.cart.items
+         })
+         .addCase(deleteUserCartItem.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.payload;
+         })
+         // CLEAR USER CART:
+         .addCase(clearUserCart.pending, (state) => {
+            state.isLoading = true;
+         })
+         .addCase(clearUserCart.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.cartItems = [];
+            state.count = 0;
+         })
+         .addCase(clearUserCart.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.payload;
+         })
+         
    }
 })
 
