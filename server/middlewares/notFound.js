59c974e5210fb8bea63eb396e041a0e5b3d2ef77
anas <anasrehman390@gmail.com>
2026-08-10 22:51:04 +0500

    fix(error): fix spelling in notFound middleware
    Fix console.log('Resource not fount') to console.log('Resource not found').

--- server/middlewares/notFound.js
+++ server/middlewares/notFound.js
@@ -1,4 +1,4 @@
 export default function notFound(req, res, next) {
-   console.log('Resource not fount');
+   console.log('Resource not found');
    return res.status(404).json({success: false, msg: 'Resource not found'});
 }
