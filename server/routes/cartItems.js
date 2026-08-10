6e4ee5608629dc2757c6fe6f93d55dea83ae2315
anas <anasrehman390@gmail.com>
2026-08-10 22:43:09 +0500

    feat(route): add routes for cart items
    Add cartItems.js callbacks to the routes to listen for the client side
    cartSlice async thunks for GET,POST,DELETE requests.

--- server/routes/cartItems.js
+++ server/routes/cartItems.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { createProduct, getProduct, deleteProduct } from '../controllers/cartItems.js';
+import { createAndUpdateProduct, getUserCart, deleteProduct, clearUserCart } from '../controllers/cartItems.js';
 const cartRouter = express.Router();
 
-cartRouter.route('/').post(createProduct)
-cartRouter.route('/:productId').get(getProduct).delete(deleteProduct)
+cartRouter.route('/').get(getUserCart).delete(clearUserCart)
+cartRouter.route('/:productId').post(createAndUpdateProduct)
+cartRouter.route('/:productId').delete(deleteProduct)
 
 export default cartRouter
