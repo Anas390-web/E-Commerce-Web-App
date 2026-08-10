31fc5d591e55134fe638defc610f611bd6670d92
anas <anasrehman390@gmail.com>
2026-08-10 22:52:38 +0500

    feat(app): Add security to app.js
    Add helmet and cross-origin access only to the public static images.
    Add rate limiter to handle too many user requests.
    Add xss to avoid cross-origin-scripting vulnerability.
    Update cors options.

--- server/app.js
+++ server/app.js
@@ -6,24 +6,55 @@ import authRouter from './routes/users.js';
 import cartRouter from './routes/cartItems.js';
 import authentication from './middlewares/auth.js';
 import cors from 'cors'
+import { xss } from 'express-xss-sanitizer';
+import rateLimiter from 'express-rate-limit';
+import helmet from 'helmet'
 import notFoundMiddlerware from './middlewares/notFound.js';
 import errorHandlerMiddleware from './middlewares/errorHandlerMiddleware.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const corsOptions = {
+   origin: process.env.REACT_ORIGIN || 'http://localhost:5173',
+   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+   credentials: true
+}
+// MUST BE SET BEFORE RATE LIMITING:
+app.set('trust proxy', 1);
+
+// SECURITY:
+app.use(helmet());
+// ALLOW CROSS-ORIGIN ACCESS ONLY FOR PUBLIC STATIC IMAGES:
+app.use(
+  '/images',
+  helmet.crossOriginResourcePolicy({ policy: "cross-origin" }),
+  express.static('public/images')
+);
+
+app.use(cors(corsOptions));
+
+// FOR HANDLING TOO MANY REQUESTS:
+const limiter = rateLimiter({
+   windowMs: 15 * 60 * 1000,
+   limit: 100,
+   standardHeaders: 'draft-8',
+   legacyHeaders: false,
+   ipv6Subnet: 56
+})
+app.use(limiter);
+
 // MIDDLEWARES:
 // PARSING JSON DATA:
 app.use(express.json());
-app.use(cors({origin: process.env.REACT_ORIGIN}))
-app.use(express.static('public'));
+app.use(xss());
 
-// routes:
+// ROUTES:
 app.use('/api/v1/products', router);
 app.use('/api/v1/users', authRouter);
 app.use('/api/v1/cartProducts', authentication, cartRouter);
 
-// error Handlers: order of code matters and 4 argument function will be triggered for server error:
+// ERROR HANDLERS: order of code matters and 4 argument function will be triggered for server error:
 app.use(notFoundMiddlerware);
 app.use(errorHandlerMiddleware);
 
