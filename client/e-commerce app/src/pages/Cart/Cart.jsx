5adb2bf0ff035a102f0f385da2ed0b56806195df
anas <anasrehman390@gmail.com>
2026-08-11 04:11:30 +0500

    feat(pages/cart): add async thunks to fetch data
    Create two functions which handle frontend increment decrement of cart
    items and dispatch the saveUserCartItem() to update the quantity in
    backend as well.
    Change properties of cartProducts from frontend defined to backend
    Schema defined (e.g: .id to .product._id etc.)
    Create two functions which show payment card and payment successful upon
    clicking PAY on cart page and YES on payment card.

--- client/e-commerce app/src/pages/Cart/Cart.jsx
+++ client/e-commerce app/src/pages/Cart/Cart.jsx
@@ -1,19 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './cart.css'
 import { Delete } from '../../icons/Icons.jsx'
 import { useDispatch, useSelector } from 'react-redux'
-import { clearCart, decrement, deleteItem, increment, selectCartItemWithProductDetails } from '../../features/Cart/cartSlice.js'
+import { clearCart, decrement, deleteItem, increment } from '../../features/Cart/cartSlice.js'
 import { Link } from 'react-router'
+import { getUserCart, saveUserCartItem, deleteUserCartItem, clearUserCart } from '../../features/Cart/cartSlice.js'
+import { PaymentCard, PaymentSuccessful } from './Payment-Card/PaymentCard.jsx'
 
 function Cart() {
    const dispatch = useDispatch();
+   const [paymentOpen, setPaymentOpen] = useState(false);
+   const [confirmPaymentOpen, setConfirmPaymentOpen] = useState(false);
+   function openPaymentPopUp() {
+      setPaymentOpen(true);
+   }
+   function cancelPaymentPopUp() {
+      setPaymentOpen(false);
+   }
+   function yesConfirmPaymentPopUp() {
+      setConfirmPaymentOpen(true);
+      setPaymentOpen(false);
+   }
+   function closeConfirmPaymentPopUp() {
+      setConfirmPaymentOpen(false);
+   }
+   
+   // NO NEED TO USE IT NOW, WILL SHOW PRODUCT ITEM'S DETAILS FROM BACKEND:
    // CALLING STATES MERGER SELECTOR FUNCTION TO GET THE MERGED STATE DATA (which is not actually merged but explicitly merged to input our cart data)
-   const cartProducts = useSelector(selectCartItemWithProductDetails);
+   // const cartProducts = useSelector(selectCartItemWithProductDetails);
+
+   // TO INCREASE/DECREASE THE CART PRODUCTS COUNT:
+   function handleProductIncrementQuantity(productId, productCount) {
+      dispatch(saveUserCartItem({
+         productId,
+         productCount: productCount + 1
+      }))
+   }
+
+   function handleProductDecrementQuantity(productId, productCount) {
+      // Don't allow quantity to drop below 1
+      if (productCount <= 1) {
+         return;
+      }
+      dispatch(saveUserCartItem({
+         productId,
+         productCount: productCount - 1
+      }))
+   }
+
+   // TO DELETE THE CART ITEM:
+   function handleProductDelete(productId) {
+      dispatch(deleteUserCartItem({ productId }))
+   }
+   // ACCESSING CART STATE FROM STORE:
+   const cartProducts = useSelector((store) => store.cart.cartItems);
 
    // FOR TOTAL AMOUNT:
    let subtotal = 0;
-   const money = cartProducts.map((product) => {
-      return subtotal += (product.price * product.productCount)
+   const money = cartProducts.map((cartProduct) => {
+      return subtotal += (cartProduct.product.price * cartProduct.quantity)
    })
    const shippingFee = 6.99;
    const totalAmount = subtotal > 0 ? subtotal + shippingFee : 0;
@@ -26,30 +71,30 @@ function Cart() {
                   <h2>Shopping Cart</h2>
                   <hr />
                </div>
-               { 
+               {
                   cartProducts.map((cartProduct) => {
                      return (
-                        <div key={cartProduct._id} className='cart-item-details'>
+                        <div key={cartProduct.product._id} className='cart-item-details'>
                            <div className='item-image'>
-                              <img src={`http://localhost:3000${cartProduct.img}`} alt={cartProduct.name} />
+                              <img src={`${import.meta.env.VITE_SERVER_URL}${cartProduct.product.img}`} alt={cartProduct.product.name} />
                            </div>
                            <div className='item-details'>
                               <div className='item-title'>
-                                 <p>{cartProduct.name}</p>
+                                 <p>{cartProduct.product.name}</p>
                               </div>
                               <div className='stock'>
                                  <p>In Stock</p>
                               </div>
                               <div className='item-price'>
-                                 ${cartProduct.price}
+                                 ${cartProduct.product.price}
                               </div>
                               <div className='add-or-delete'>
-                                 <button onClick={() => dispatch(decrement(cartProduct._id))} className='delete-icon'><Delete /></button>
-                                 <p>{cartProduct.productCount}</p>
-                                 <button onClick={() => dispatch(increment(cartProduct._id))}>+</button>
+                                 <button onClick={() => (handleProductDecrementQuantity(cartProduct.product._id, cartProduct.quantity))} className='delete-icon'><Delete /></button>
+                                 <p>{cartProduct.quantity}</p>
+                                 <button onClick={() => (handleProductIncrementQuantity(cartProduct.product._id, cartProduct.quantity))}>+</button>
                               </div>
                               <div className='delete-btn'>
-                                 <button onClick={() => dispatch(deleteItem(cartProduct._id))}>Delete</button>
+                                 <button onClick={() => handleProductDelete(cartProduct.product._id)}>Delete</button>
                               </div>
                            </div>
                         </div>
@@ -64,7 +109,7 @@ function Cart() {
                      </div>
                   </Link>
                   <div className='clear-cart'>
-                     <button onClick={() => dispatch(clearCart())}>Clear Cart</button>
+                     <button onClick={() => dispatch(clearUserCart())}>Clear Cart</button>
                   </div>
                </div>
                <div className='total-payment-box'>
@@ -78,9 +123,17 @@ function Cart() {
                         <p>Total Amount: ${totalAmount.toFixed(2)}</p>
                      </div>
                   </div>
-                  <div className='login-btn'>
-                     <button>LOGIN</button>
+                  <div className='pay-btn'>
+                     <button onClick={openPaymentPopUp}>PAY</button>
                   </div>
+                  {
+                     paymentOpen &&
+                     <PaymentCard cancelPaymentPopUp={cancelPaymentPopUp} yesConfirmPaymentPopUp={yesConfirmPaymentPopUp} />
+                  }
+                  {
+                     confirmPaymentOpen &&
+                     <PaymentSuccessful closeConfirmPaymentPopUp={closeConfirmPaymentPopUp} />
+                  }
                </div>
             </article>
          </section>
