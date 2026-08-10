f0af297a9b877fb2bddd5dcbef4eeeebb3ddf280
anas <anasrehman390@gmail.com>
2026-08-10 21:37:53 +0500

    feat(Layout): dispatch thunks
    Dispatch authSlice fetchUserDetails() thunk to show username on the NavBar if
    token is present.
    Dispatch cartSlice getUserCart() thunk to show cart items on every
    screen refresh if the token is present.

--- client/e-commerce app/src/Components/Layout/Layout.jsx
+++ client/e-commerce app/src/Components/Layout/Layout.jsx
@@ -1,8 +1,21 @@
 import { Outlet } from 'react-router'
 import NavBar from '../NavBar/NavBar'
 import Footer from '../Footer/Footer'
+import { useEffect } from 'react';
+import { getUserCart } from '../../features/Cart/cartSlice';
+import { useDispatch } from 'react-redux';
+import { fetchUserDetails } from '../../features/Authentication/authSlice';
 
 export default function Layout() {
+    const dispatch = useDispatch();
+    // TO DISPLAY USERNAME =  CART ITEMS UPON RE-LOADING PAGE:
+    const token = localStorage.getItem('accessToken');
+    useEffect(() => {
+        if(token) {
+            dispatch(fetchUserDetails());
+            dispatch(getUserCart());
+        }
+    }, [])
     return (
         <div>
             <NavBar />
